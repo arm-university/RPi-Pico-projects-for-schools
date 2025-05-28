bebe88436bef6166602ca3b63deb6346597d1b4b
Haremi <skenney@haremi.co.uk>
2025-05-28 13:39:16 +0100

    Add files via upload

---
+++ RPi-Pico-projects-for-schools/CH9/9.1Servo.py
@@ -0,0 +1,20 @@
+from machine import Pin, PWM
+from utime import sleep_ms
+
+MID = 1500000
+MIN = 1000000
+MAX = 2000000
+SERVOPIN = 15
+SERVOFREQ = 50
+
+servo = PWM(Pin(SERVOPIN))
+servo.freq(SERVOFREQ)
+servo.duty_ns(MID) #Initialize to middle position
+sleep(1)
+
+while True:
+    servo.duty_u16(MIN) #Move to minimum
+    sleep(1)
+    servo.duty_u16(MID) #Back to middle
+    sleep(1)
+    servo.duty_u16(MAX) #Move to maximum
