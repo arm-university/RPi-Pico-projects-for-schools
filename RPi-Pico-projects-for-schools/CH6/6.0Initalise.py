d62e73274ddd258ee40e9f4a3a5a2cd317bb34fd
tomcarter868 <tom.carter@arm.com>
2025-06-02 14:21:26 +0100

    Delete RPi-Pico-projects-for-schools/CH6/6.0Initalise.py

--- RPi-Pico-projects-for-schools/CH6/6.0Initalise.py
+++
@@ -1,9 +0,0 @@
-from machine import I2C, Pin
-from icm20948 import ICM20948
-
-# Initialize I2C
-i2c = I2C(0, scl=Pin(21), sda=Pin(20))
-
-# Initialize the ICM20948 sensor
-imu = ICM20948(i2c_addr=0x68, i2c_bus=i2c)
-print("ICM20948 initialized successfully.")
